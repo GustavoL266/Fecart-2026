10d2ce1a45c363e5320246e8df46504c5f675a34
luka <lucca.frazili@edu.fecap.br>
2026-08-26 22:10:18 -0300

    Redireciona GitHub Pages para aplicação publicada

--- file-protocol-redirect.js
+++ file-protocol-redirect.js
@@ -1,3 +1,9 @@
-if (window.location.protocol === "file:") {
+const hostedAppUrl = "https://fecart-2026.onrender.com/";
+const isProjectGithubPages =
+  window.location.hostname === "gustavol266.github.io" && window.location.pathname.startsWith("/Fecart-2026");
+
+if (isProjectGithubPages) {
+  window.location.replace(`${hostedAppUrl}${window.location.hash}`);
+} else if (window.location.protocol === "file:") {
   window.location.replace("http://localhost:3000/");
 }
